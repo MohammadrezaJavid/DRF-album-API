c455c8d8585585cbf9ebe346ac4d33ab49929fcb
mohammadrezajavid <javid.9@yahoo.com>
2023-09-21 10:51:46 +0330

    seprate databases info from settings.py file to env.py

--- album/album/settings.py
+++ album/album/settings.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 import os
 from datetime import timedelta
+from ..env import Env
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-an*^cnfc7=ucx@94g7cgp^sjw1nmv)50ng1f5#2@7)so11@4op'
@@ -92,14 +93,15 @@ SWAGGER_SETTINGS = {
 #     }
 # }
 
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'album',
-        'USER': 'javid',
-        'PASSWORD': 'javid_data9999',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'NAME': Env.NAME,
+        'USER': Env.USER,
+        'PASSWORD': Env.PASSWORD,
+        'HOST': Env.HOST,
+        'PORT': Env.PORT,
     },
 }
 
